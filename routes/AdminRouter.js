9f73fe0bb9f6fe523d49e689ae6a28a3926e870b
oscar <oscar.burman005@gmail.com>
2025-03-23 20:20:02 +0100

    Brought back imageValidator

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const multer = require("multer");
 const upload = multer();
 const { ensureNewProfile, ensureUniqueProfile } = require("../middlewares/Profile");
+const { validateImage } = require("../validators/imageValidator");
 const { validateProfile } = require("../validators/profileValidator");
 const { validateEvent } = require("../validators/eventValidator");
 const { createProfile, editProfile, getProfile } = require("../controllers/ProfileController");
@@ -14,49 +15,31 @@ const {
 } = require("../controllers/EventController");
 const { validateProject } = require("../validators/projectValidator");
 const { editProject, addProject, deleteProject } = require("../controllers/ProjectContoller");
-// const { validateImage } = require("../validators/imageValidator");
 
 router.get("/profile", getProfile);
 router.get("/eventquestions", getEventQuestions);
 router.get("/eventenrollments", getEventEnrollments);
 
-// router.post(
-//   "/createprofile",
-//   upload.single("profileImage"),
-//   validateProfile,
-//   ensureNewProfile,
-//   validateImage,
-//   createProfile
-// );
 router.post(
   "/createprofile",
   upload.single("profileImage"),
   validateProfile,
   ensureNewProfile,
+  validateImage,
   createProfile
 );
-// router.post("/addevent", upload.single("image"), validateEvent, validateImage, addEvent);
-router.post("/addevent", upload.single("image"), validateEvent, addEvent);
+router.post("/addevent", upload.single("image"), validateEvent, validateImage, addEvent);
 router.post("/addproject", validateProject, addProject);
 
-// router.put(
-//   "/editprofile",
-//   upload.single("profileImage"),
-//   validateProfile,
-//   ensureUniqueProfile,
-//   validateImage,
-//   editProfile
-// );
 router.put(
   "/editprofile",
   upload.single("profileImage"),
   validateProfile,
   ensureUniqueProfile,
+  validateImage,
   editProfile
 );
-// router.put("/editevent", upload.single("image"), validateEvent, validateImage, editEvent);
-router.put("/editevent", upload.single("image"), validateEvent, editEvent);
-
+router.put("/editevent", upload.single("image"), validateEvent, validateImage, editEvent);
 router.put("/editproject", validateProject, editProject);
 
 router.delete("/deleteevent", deleteEvent);
