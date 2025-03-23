6eb5ac55c1df72d821eec5e96d796b7100a13a44
oscar <oscar.burman005@gmail.com>
2025-03-23 20:34:47 +0100

    Fixed missing express import

--- routes/ProxyRouter.js
+++ routes/ProxyRouter.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("path");
 
