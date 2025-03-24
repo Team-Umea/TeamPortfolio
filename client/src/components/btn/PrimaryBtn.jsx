32176f48c85724edb3082e32cc029adc1f9a68e1
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-24 19:09:18 +0100

    changed color and hovereffect on PrimaryBtn, now changes to a brighter color instead of decreasing opacity.

--- client/src/components/btn/PrimaryBtn.jsx
+++ client/src/components/btn/PrimaryBtn.jsx
@@ -3,7 +3,8 @@ export default function PrimaryBtn({ type = "button", onClick, children }) {
     <button
       type={type}
       onClick={onClick}
-      className="flex justify-center items-center w-full gap-x-2 px-6 py-2 text-white rounded-full bg-gray-600 transition-all duration-300 ease hover:opacity-70 shadow-xl cursor-pointer">
+      className="flex justify-center items-center w-full gap-x-2 px-6 py-2 text-white rounded-full bg-blue-700 transition-all duration-300 ease hover:bg-blue-600 shadow-xl cursor-pointer"
+    >
       {children}
     </button>
   );
