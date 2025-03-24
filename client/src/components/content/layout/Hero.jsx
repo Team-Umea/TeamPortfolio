b051e66e04ed346964156b178dea4e021e3b9627
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-24 17:22:01 +0100

    Hero is pretty much done, it's responsive and looks OK, changed title and language on index.html

--- client/src/components/content/layout/Hero.jsx
+++ client/src/components/content/layout/Hero.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { Link } from "react-router";
+import PrimaryBtn from "../../btn/PrimaryBtn";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-8 h-screen">
-      <h1 className="text-5xl md:text-6xl font-semibold">Team Umeå</h1>
-      <h2 className="text-xl md:text-2xl">Möt framtidens utvecklare</h2>
-    </div>
+    <section
+      className="relative flex overflow-hidden w-full h-auto min-h-60 sm:min-h-100 xl:min-h-screen justify-center items-center text-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/heroportfolio.webp')" }}
+    >
+      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="relative z-10 text-white px-6 max-w-lg md:max-w-2xl">
+        <h1 className="text-3xl md:text-5xl font-bold">Team Umeå</h1>
+        <h2 className="text-lg md:text-2xl m-2">Möt framtidens utvecklare</h2>
+        <Link to="/events">
+          <PrimaryBtn>Kommande evenemang</PrimaryBtn>
+        </Link>
+      </div>
+    </section>
   );
 }
