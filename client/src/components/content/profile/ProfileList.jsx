c104f29fd97c8940c8627d7feb138a1e70dfbadd
Neriman <neriman@live.se>
2025-03-31 14:34:57 +0200

    testimonials

--- client/src/components/content/profile/ProfileList.jsx
+++ client/src/components/content/profile/ProfileList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import { AnimatedTestimonials } from "../../ui/animated-testimonials";
 
 export default function ProfileList({ profiles }) {
   return (
     <ul className="flex flex-wrap gap-12">
+      <AnimatedTestimonials testimonials={profiles} autoplay={ false} />
       {profiles.map((profile) => {
         return <ProfileCard key={profile._id} profile={profile} />;
       })}
