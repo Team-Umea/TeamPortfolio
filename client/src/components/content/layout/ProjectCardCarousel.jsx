4c63f74b4f3100f67b9695fa9f050f2833f0f887
oscar <oscar.burman005@gmail.com>
2025-04-01 16:15:43 +0200

    Fixed profile form issue with updating profile img

--- client/src/components/content/layout/ProjectCardCarousel.jsx
+++ client/src/components/content/layout/ProjectCardCarousel.jsx
@@ -1,31 +1,21 @@
-"use client";
-import { Img } from "react-image";
-import React, { useEffect } from "react";
 import { Carousel, Card } from "../ui/project-card-carousel";
 import useContentStore from "@/hooks/useContentStore";
 
 export function ProjectCardCarousel() {
-  const {projects} = useContentStore();
-  // const cards = data.map((card, index) => (
-  //   <Card key={card.src} card={card} index={index} />
-  // ));
+  const { projects } = useContentStore();
 
   const cards = projects.map((project, index) => (
     <Card
       key={project._id}
       card={{
-        category: project.techStack?.join(", ") || "Unknown", // Convert tech stack to a string
+        category: project.techStack?.join(", ") || "Unknown",
         title: project.project,
-        src: project.images?.[1]?.url || project.images?.[0]?.url, // Fallback image
-        description: project.description
+        src: project.images?.[1]?.url || project.images?.[0]?.url,
+        description: project.description,
       }}
       index={index}
     />
   ));
-
-  useEffect(() => {
-    console.log(projects);
-  });
 
   return (
     <div className="w-full h-full">
